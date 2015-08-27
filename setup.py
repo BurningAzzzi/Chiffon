346a1502c5675175f1fda704b482307e24590ba9
eleven <iGod_eleven@163.com>
2015-08-27 23:42:28 +0800

    finish release 0.0.1

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+# Date  : 2015-08-27
+# Author: Master Yumi
+# Email : yumi@meishixing.com
+
+from version import VERSION
+try:
+    from setuptools import setup
+except ImportError:
+    pass
+
+setup(
+    name = 'chiffon',
+    version = VERSION,
+    keywords = ('api frome work', 'chiffon'),
+    description = 'The API Framework For THack',
+    author = 'Mr.Eleven',
+    packages = [ '.', 'chiffon'], 
+    )
+
